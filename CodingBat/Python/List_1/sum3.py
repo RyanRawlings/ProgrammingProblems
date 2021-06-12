14216035a72937a1be1645cef3357b68fc66a5f4
Ryan Rawlings <ryan.t.rawlings@student.uts.edu.au>
2021-06-12 14:29:16 +1000

    Complete sum3 python problem

---
+++ CodingBat/Python/List_1/sum3.py
@@ -0,0 +1,12 @@
+# **********************************************************************************
+#   Title: List-1 > sum3
+#   URL: https://codingbat.com/prob/p191645
+#   Date: 12/06/2021
+#   Description: 
+#     - Given an array of ints length 3, return the sum of all the elements.
+#************************************************************************************
+
+def sum3(nums):
+    return sum(nums)
+    
+print(sum3([1,2,3]))
