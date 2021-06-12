7562f151cec1e9e3561fd5b25b011aa2926d24c9
Ryan Rawlings <ryan.t.rawlings@student.uts.edu.au>
2021-06-12 14:03:36 +1000

    Finish Warmup-2 problem set, create directory for List-1 problem set, complete first_last6 python problem

---
+++ CodingBat/Python/List_1/first_last6.py
@@ -0,0 +1,13 @@
+# **********************************************************************************
+#   Title: Warmup-2 > first_last6
+#   URL: https://codingbat.com/prob/p181624
+#   Date: 12/06/2021
+#   Description: 
+#     - Given an array of ints, return True if 6 appears as either the 
+#       first or last element in the array. The array will be length 1 or more.
+#************************************************************************************
+
+def first_last6(nums):
+  return nums[0] == 6 or nums[len(nums)-1] == 6
+
+print(first_last6([1,2,3,6]))
